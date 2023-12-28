5fa0ceb78b67615f6461a72de62e909d87b7fe7d
Crasherlol <cadudu2012@hotmail.com>
2023-12-28 00:20:15 -0300

    jest

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,22 +1,24 @@
 const {hash, compare} = require("bcryptjs");
-const AppError = require('../utils/AppError')
+const AppError = require('../utils/AppError');
 
-
-const sqliteConnection = require('../database/sqlite')
+const UserRepository = require("../repositories/UserRepository");
+const sqliteConnection = require('../database/sqlite');
 
 
 
 class UserController {
     async create(request, response){
         const {name, email, password} = request.body;
-        const database = await sqliteConnection();
-        const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+        const userRepository = new UserRepository()
+        const checkUserExist = await userRepository.findByEmail(email)
 
         if (checkUserExist){
             throw new AppError("Este email ja está em uso.")
         }
         const hashedPassword = await hash(password, 8);
-        await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]);
+
+        await userRepository.create({name, email, password: hashedPassword})
 
         return response.status(201).json();
 
